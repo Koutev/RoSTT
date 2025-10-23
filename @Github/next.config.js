e221c41ad23641ff630d4e73dc36788779fa45cf
Koutev <69212053+Koutev@users.noreply.github.com>
2025-10-23 10:28:57 -0300

    Delete @Github directory

--- @Github/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // App Router ya no es experimental en Next.js 14
-  output: 'standalone', // Optimización para Vercel
-}
-
-module.exports = nextConfig
